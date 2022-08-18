47350c888bb8618484029f8ec1405377ad0e348c
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-08-17 20:08:40 -0500

    Impl 04/fill/Fill.asm

--- justfile
+++ justfile
@@ -4,3 +4,11 @@
 # Open ./tools/HardwareSimulator.sh
 @hardware:
     ./tools/HardwareSimulator.sh &> /dev/null
+
+# Open ./tools/Assembler.sh
+@assembler:
+  ./tools/Assembler.sh &> /dev/null
+
+# Open ./tools/CPUEmulator.sh
+@cpu:
+  ./tools/CPUEmulator.sh
--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,64 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+  // Check if a key was pressed 
+  D = M [24576]
+
+  // If it was, set screen to black
+  D;JNE   [IF]
+  // Else set to white
+  0;JMP [ELSE]
+  
+  // Actual if-else 
+  (IF) // If key, set ARG = 1    
+      // Skip if ARG is already 0
+      D = M [ ARG]
+      D;JEQ [LOOP]
+
+      D = A [   0]
+      M = D [ ARG]
+
+      // "Call" Set 
+      0;JMP [SET]
+
+  (ELSE) // Else set ARG = 0
+      // Skip if ARG is already 1
+      D = M [ ARG]
+      D = D - 1
+      D;JEQ [LOOP]
+
+      D = A [   1]
+      M = D [ ARG] 
+      
+      // "Call" Set 
+      0;JMP [SET]
+    
+// Set "function"
+(SET)
+    // Loop trough all screen bits
+    // Set THIS = SCREEN
+    D = A   [SCREEN]
+    M = D   [THIS]
+
+    // Set i (THAT) = 8192
+    D = A   [8192]
+    M = D   [THAT]
+
+    (SETLOOP)
+      D = M [ ARG]
+      A = M [THIS]
+      M = D - 1 
+      
+      // i (THIS) += 1
+      MD = M + 1 [THIS]
+      // (THAT) -= 1
+      MD = M - 1 [THAT]
+
+      // Continue while i () >= 0
+      D;JGE [SETLOOP]
+      
+    // Go to LOOP 
+    0;JMP [LOOP]
+
